bc1949acc605d8d7e2e560c40a3118be36ed123c
zsmester <zsolt.mester67@gmail.com>
2016-11-11 10:46:58 +0100

    Remove windows related code.

--- intro/main.cpp
+++ intro/main.cpp
@@ -1,12 +1,8 @@
-// Valaszd ki az op. rendszert:
-//#define WINDOWS
-#define LINUX
-// ----------------------------
 #define PORT 42002
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
-#ifdef LINUX
+
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,19 +10,11 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
-#endif
-#ifdef WINDOWS
-#define _WIN32_WINNT 0x501
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <windows.h>
-#endif
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-#ifdef LINUX
 	int iResult;
     int server = socket(AF_INET,SOCK_STREAM,0);
 	if(server == -1) {
@@ -34,27 +22,6 @@ int main(int argc, char **argv)
   		return -1;
 	}
 	int sockError = -1;
-#endif
-#ifdef WINDOWS
-	WSADATA wsaData;
-    int iResult;
-    /// windows socket inicializalasa
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed: %d\n", iResult);
-        return 1;
-    } else {
-        cout << " >> Winsock DLL is ready to use." << endl;
-    }
-    DWORD dwError;
-
-    SOCKET server = socket(AF_INET,SOCK_STREAM,0);
-	if(server == INVALID_SOCKET) {
-  		cerr << "Opening stream socket failed. ERROR:" << WSAGetLastError() << endl;
-  		return -1;
-	}
-	int sockError = SOCKET_ERROR;
-#endif
 
 	struct sockaddr_in server_name;
 	server_name.sin_family = AF_INET;
@@ -64,12 +31,7 @@ int main(int argc, char **argv)
     int _set=1;
     iResult = setsockopt(server, SOL_SOCKET, SO_REUSEADDR, (char *) &_set, sizeof(int));
     if (iResult == sockError) {
-#ifdef LINUX
         cerr << "The call of setsockopt failed with error:" << errno << endl;
-#endif
-#ifdef WINDOWS
-        cerr << "The call of setsockopt failed with error:" << WSAGetLastError() << endl;
-#endif
     } else {
         cerr << "\t\tSet SO_REUSEADDR: ON" << endl;
     }
@@ -107,43 +69,21 @@ int main(int argc, char **argv)
         // Blokkol, am�g nincs beerkezo hivas.
         struct sockaddr_in caller;
         socklen_t addrlen = sizeof(caller);
-#ifdef LINUX
         int call = accept(server,(struct sockaddr *) &caller, &addrlen);
-#endif
-#ifdef WINDOWS
-        SOCKET call = accept(server,(struct sockaddr *) &caller, &addrlen);
-#endif
         int bytecontroll=send(call,responseMessage1.c_str(),strlen(responseMessage1.c_str()),0);
         if(bytecontroll==sockError) {
-#ifdef LINUX
             cout << " Send failed with error: " << errno << endl;
             close(call);
-#endif
-#ifdef WINDOWS
-            cout << " Send failed with error: " << WSAGetLastError();
-            closesocket(call);
-#endif
         } else if (bytecontroll==strlen(responseMessage1.c_str())) {
             char* puffer=new char[200];
             memset(puffer, '\0', 200);
             bytecontroll=recv(call, puffer, 200, 0);
             if(bytecontroll==0) {
                 cout << "Connection closed by peer." << endl;
-#ifdef LINUX
                 close(call);
-#endif
-#ifdef WINDOWS
-                closesocket(call);
-#endif
             }else if(bytecontroll<0){
-#ifdef LINUX
                 cout << "The recv call failed with error: " << errno << endl;
                 close(call);
-#endif
-#ifdef WINDOWS
-                cout << "The recv call failed with error: " << WSAGetLastError() << endl;
-                closesocket(call);
-#endif
             } else if (bytecontroll < 200) {
                 puffer[bytecontroll]='\0';
                 struct hostent *remoteHost;
@@ -152,12 +92,7 @@ int main(int argc, char **argv)
                 host_name=puffer;
                 if(host_name == "/q") {
                      cout << "Exit was requested by user. " << endl;
-#ifdef LINUX
                      close(call);
-#endif
-#ifdef WINDOWS
-                     closesocket(call);
-#endif
                      break;
                 }
 
@@ -166,12 +101,7 @@ int main(int argc, char **argv)
                 socklen_t length = sizeof(call_info);
                 if( getsockname( call, (struct sockaddr *) &call_info, &length) == -1 ) {
                     cerr << "Getting socket failed." << endl;
-#ifdef LINUX
 					close(call);
-#endif
-#ifdef WINDOWS
-					closesocket(call);
-#endif
                     continue;
                 }
 
@@ -185,7 +115,6 @@ int main(int argc, char **argv)
                 remoteHost = gethostbyname(host_name.c_str());
                 ostringstream responseTmp;
                 if (remoteHost == NULL) {
-#ifdef LINUX
                     if (h_errno != 0) {
                         if (h_errno == HOST_NOT_FOUND) {
                             responseTmp << "Host not found" << '\n';
@@ -195,19 +124,6 @@ int main(int argc, char **argv)
                             responseTmp << "Function failed with error: " << h_errno << '\n';
                         }
                     }
-#endif
-#ifdef WINDOWS
-                    dwError = WSAGetLastError();
-                    if (dwError != 0) {
-                        if (dwError == WSAHOST_NOT_FOUND) {
-                            responseTmp << "Host not found" << '\n';
-                        } else if (dwError == WSANO_DATA) {
-                            responseTmp << "No data record found" << '\n';
-                        } else {
-                            responseTmp << "Function failed with error: " << dwError << '\n';
-                        }
-                    }
-#endif
                 } else {
                     responseTmp << "Function returned:\n";
                     responseTmp << "\tOfficial name: " << remoteHost->h_name << '\n';
@@ -254,36 +170,9 @@ int main(int argc, char **argv)
             }
             delete puffer;
         }
-#ifdef LINUX
         close(call);
-#endif
-#ifdef WINDOWS
-        closesocket(call);
-#endif
     }
-#ifdef LINUX
     close(server);
 	cout << " << Server is now terminating." << endl;
-#endif
-#ifdef WINDOWS
-    closesocket(server);
-	if(WSACleanup()!=0) {
-        dwError=WSAGetLastError();
-        if (dwError == WSANOTINITIALISED) {
-            cout << "A successful WSAStartup call must occur before using this function." << endl;
-            return 1;
-        } else if (dwError == WSAENETDOWN) {
-            cout << "The network subsystem has failed." << endl;
-            return 1;
-        } else if (dwError == WSAENETDOWN) {
-            cout << "A blocking Windows Sockets 1.1 call is in progress, or the service provider is still processing a callback function." << endl;
-            return 1;
-        } else {
-            cout << "Function failed with error: " << dwError << endl;
-            return 1;
-        }
-    }
-    cout << " << Winsock DLL is closed." << endl;
-#endif
     return 0;
 }
